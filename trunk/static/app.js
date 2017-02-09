115d215af7614cbd11d94b923132467548f5de85
katia.cristina <katia.cristina@91449c27-e80b-5b4a-865a-59dad3fa3145>
2017-02-09 16:16:20 +0000

    + Adicionado comentários em algumas partes do código.

--- trunk/static/app.js
+++ trunk/static/app.js
@@ -1,6 +1,8 @@
+
 var app = angular.module("FormController",["schemaForm"]);
 app.controller("form", function($scope, $http) { 
-  $scope.tests = [
+  
+  $scope.availableTemplates = [
   { name: "Vitrine do Artesanato - Boas Vindas", data:"boasvindas"},
   { name: "Vitrine do Artesanato - Dicas de Artesanatos", data: "dicasdeartesanato" },
   { name: "Vitrine do Artesanato - Lançamentos", data: "lancamentos" },
@@ -9,7 +11,9 @@ app.controller("form", function($scope, $http) {
   ]; 
   $scope.modelData = {};
   $scope.decorator = 'bootstrap-decorator';
-  $scope.processModel = function(){
+
+ 
+  $scope.processModel = function(){    
     try {
       $scope.result = $scope.template($scope.modelData);
       $("#preview").contents().find("body").html($scope.result);
@@ -18,19 +22,26 @@ app.controller("form", function($scope, $http) {
       $("#error").text(e.message).show();
     }  
   };
+
   $scope.renderCode = function(){
     $("#modalResult").modal()
   };
-  $scope.$watch("selectedTest",function(val){
+
+  
+  $scope.$watch("selectTemplate",function(val){
     if (!val || val.data == undefined)
 	  return;	
     $("#error").hide();
+
+    
     var r =  Math.round(Math.random() * 1024);
     $http.get('data/' + val.data + '/schema.js?r=' + r).then(function (res){ $scope.schema = res.data;});
     $http.get('data/' + val.data + '/form.js?r=' + r).then(function (res){ $scope.form = res.data;});   
     $http.get('data/' + val.data + '/template.html?r=' + r).then(function(res){ $scope.template = Handlebars.compile(res.data);}) 
   });  
-})
+}) 
+
+
 Handlebars.registerHelper("eachItem", function(items, options){
  var result = "";
  for(var i=0; i<items.length; i++)
@@ -44,6 +55,8 @@ Handlebars.registerHelper("eachItem", function(items, options){
  }     
  return result;
 }); 
+
+
 Handlebars.registerHelper('if_even', function(element, options) {
   if(options != null && options.data != null && options.data.index % 2 == 0)
     return options.fn(this);

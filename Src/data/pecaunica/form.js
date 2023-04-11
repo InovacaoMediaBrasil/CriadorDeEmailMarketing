dd48278488d8b3adcb6cc970762821081adc6a1d
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-04-12 01:50:38 +0400

    Fix location

---
+++ Src/data/pecaunica/form.js
@@ -0,0 +1,42 @@
+[{
+    "title": "Banner",
+    "key": "banner",
+    "placeholder": "Informe o endereço do banner"
+  }, 
+  {
+    "title": "Link Banner",
+    "key": "linkbanner",
+    "placeholder": "Informe o link do banner"
+  },
+  {
+    "title": "Nome do banner",
+    "key": "namebanner",
+    "placeholder": "Informe o nome do banner"
+  },
+  {
+    "title": "Data",
+    "key": "date",
+    "placeholder": "Data final da oferta"
+  },
+  {
+    "title": "Passo",
+    "key": "class",
+    "add": "Novo passo",
+    "style": {
+      "add": "btn-success"
+    },
+    "items": [{
+        "key": "class[].image",
+        "placeholder": "Caminho da imagem"
+      }, {
+        "key": "class[].title",
+        "placeholder": "passo (1, 2, 3...) e título"
+      },
+      {
+        "key": "class[].description",
+        "type": "textarea",
+        "placeholder": "Descriçao do passo"
+      }
+    ]
+  }
+]
